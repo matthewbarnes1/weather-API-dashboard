0c4d951a1aa5a4cc997e16db3aeb0bcdf14ffbc7
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-08-21 18:51:04 -0400

    Weather API fetch up and running

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,46 @@
-var APIKey = "1732282d0466ef819c6dfdd2f9bf0e5c";
+const btnInput = document.getElementById("search-button");
+btnInput.addEventListener("click", myFunction);
 
-const btnInput = document.getElementById('search-button');
+var name = document.querySelector(".name");
+var temp = document.querySelector(".temp");
+var desc = document.querySelector(".desc");
 
-btnInput.addEventListener('click', myFunction);
+var city = document.getElementById("text-input");
 
 var clickCount = 0;
 
 function myFunction() {
-    var city = document.getElementById('text-input');
+  var city = document.getElementById("text-input").value;
+  // * This is for de-bugging perposes console.log(city);
 
-        if(city.value.length == 0){
-            alert("ERROR: Input Required (City)");
-        }
-        else{
-            var city = document.getElementById('text-input').value;
-            console.log(city);
-            localStorage.setItem('cityName', city);
-            clickCount++;
-        }
-        console.log(clickCount);
+  var APIKey = "1732282d0466ef819c6dfdd2f9bf0e5c";
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
 
-        //TODO: Use city value to concatinate queryURL
+  function Fetchweather() {
+    fetch(queryURL)
+      .then((response) => response.json())
+      .then((data) => {
+        var temperature = data.main.temp;
+        var cityName = data.name;
+        var windSpeed = data.wind.speed;
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    // console.log(queryURL);
-
-    // TODO: Fetch data from Weather API using queryURL
-    
-
+        console.log("Temperature:", temperature);
+        console.log("City Name:", cityName);
+        console.log("Wind Speed:", windSpeed);
+      });
+  }
+  Fetchweather();
 }
+
+//! The below if/else can be used to display alert if input = none;
+//   if (city.value.length == 0) {
+//     alert("ERROR: Input Required (City)");
+//   } else {
+//     console.log(city);
+//     localStorage.setItem("cityName", city);
+//     clickCount++;
+//   }
